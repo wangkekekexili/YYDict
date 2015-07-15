0c7160dfc600f79bb59bf9597eaa9a52f329e018
Ke Wang <wangkekekexili@gmail.com>
2015-07-15 18:01:53 -0400

    Add Pearson.

---
+++ src/util/dict/Pearson.java
@@ -0,0 +1,11 @@
+package util.dict;
+
+import util.SearchResult;
+
+public class Pearson {
+	public static SearchResult search(String word) {
+		// TODO
+		// OuxdaFJAs3g3CVx1x3ViNNAGRDfWOIR7
+		return new SearchResult(false, "");
+	}
+}
